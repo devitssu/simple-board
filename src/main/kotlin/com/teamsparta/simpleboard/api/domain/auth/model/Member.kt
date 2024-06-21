20a18cb4aadbcaa9c6ad854ff050d05b2961d83e
itssu <ssujinn96@gmail.com>
2024-06-21 20:01:38 +0900

    🛂 MemberRole 추가

--- src/main/kotlin/com/teamsparta/simpleboard/api/domain/auth/model/Member.kt
+++ src/main/kotlin/com/teamsparta/simpleboard/api/domain/auth/model/Member.kt
@@ -1,9 +1,12 @@
 package com.teamsparta.simpleboard.api.domain.auth.model
 
 import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import org.springframework.security.crypto.password.PasswordEncoder
 
 @Entity
 class Member(
@@ -11,7 +14,14 @@ class Member(
     val nickname: String,
     val password: String,
 
+    @Enumerated(EnumType.STRING)
+    val role: MemberRole = MemberRole.MEMBER,
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long? = null
-)
+) {
+    fun checkPassword(passwordEncoder: PasswordEncoder, requestPW: String): Boolean {
+        return passwordEncoder.matches(requestPW, this.password)
+    }
+}
---
+++ src/main/kotlin/com/teamsparta/simpleboard/api/domain/auth/model/MemberRole.kt
@@ -0,0 +1,5 @@
+package com.teamsparta.simpleboard.api.domain.auth.model
+
+enum class MemberRole {
+    MEMBER, ADMIN
+}
