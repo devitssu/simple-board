da474469eb2d04300590dd61096bf6c411b8ddc9
itssu <ssujinn96@gmail.com>
2024-06-21 20:33:37 +0900

    🔧 swagger authorization header ui 추가

--- src/main/kotlin/com/teamsparta/simpleboard/infra/swagger/SwaggerConfig.kt
+++ src/main/kotlin/com/teamsparta/simpleboard/infra/swagger/SwaggerConfig.kt
@@ -1,7 +1,10 @@
 package com.teamsparta.simpleboard.infra.swagger
 
+import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.security.SecurityRequirement
+import io.swagger.v3.oas.models.security.SecurityScheme
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -15,5 +18,19 @@ class SwaggerConfig {
                 .title("Simple-board API")
                 .description("Simple-board API schema")
                 .version("1.0.0")
+        )
+        .addSecurityItem(
+            SecurityRequirement().addList("Bearer Authentication")
+        )
+        .components(
+            Components().addSecuritySchemes(
+                "Bearer Authentication",
+                SecurityScheme()
+                    .type(SecurityScheme.Type.HTTP)
+                    .scheme("Bearer")
+                    .bearerFormat("JWT")
+                    .`in`(SecurityScheme.In.HEADER)
+                    .name("Authorization")
+            )
         )
 }
