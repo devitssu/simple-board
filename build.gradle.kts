d6e0eaf16ad2fe976e1adb6ef106264e1a65482d
itssu <ssujinn96@gmail.com>
2024-06-26 16:05:59 +0900

    ➕ kotest, mockk 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,8 @@ repositories {
 
 val jjwtVersion = "0.12.5"
 val queryDslVersion = "5.0.0"
+val kotestVersion = "5.5.5"
+val mockkVersion = "1.13.8"
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
@@ -44,6 +46,11 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testImplementation("org.springframework.security:spring-security-test")
+    testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
+    testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
+    testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3")
+    testImplementation("io.mockk:mockk:$mockkVersion")
+    testImplementation("com.ninja-squad:springmockk:3.1.1")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
